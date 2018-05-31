05a15466f00ef9c25ca261bb7b3de0408161c450
Jean-Frederic Clere <jfclere@gmail.com>
2018-05-31 11:38:00 +0200

    Add the headers for mod_cache

--- servlet/src/main/java/org/jfclere/JBCS534/Servlet.java
+++ servlet/src/main/java/org/jfclere/JBCS534/Servlet.java
@@ -3,6 +3,8 @@ package org.jfclere.JBCS534;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +16,11 @@ public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String title = "mod_cache test servlet";
+		response.setHeader("ETag","W/\"5581-1509871979000\"");
+		response.setHeader("Expires", "Wed, 21 Oct 2018 07:28:00 GMT");
+		Date date = new Date();
+		response.setHeader("TraceDate", date.toString());
+
 		PrintWriter out = response.getWriter();
 		out.println("<title>" + title + "</title>");
 		out.println("</head>");
