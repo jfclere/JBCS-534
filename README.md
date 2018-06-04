29e912e5c885ad7bbbcb0a16a149d23359d4e7bb
Jean-Frederic Clere <jfclere@gmail.com>
2018-06-04 11:30:41 +0200

    Add a random payload

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # JBCS-534
 Httpd with mod_cache_disk on windows tests
+
+./etc/httpd/conf.modules.d/00-mpm.conf
+ThreadsPerChild      150
+MaxRequestsPerChild    0
+
+Note in my tests:
+Fri Jun 01 08:30:07.244602 2018] [mpm_winnt:debug] [pid 9060:tid 2208] child.c(399): AH00334: Child: Accept thread listening on [::1]:443 using AcceptFilter data
+According to the doc that makes no sense:
+none uses accept()
+connect uses AcceptEx()
+and data isn't working on windows...
+The customer uses:
+[Tue May 29 09:05:42.421584 2018] [mpm_winnt:debug] [pid 7720:tid 19492] child.c(399): AH00334: Child: Accept thread listening on [::]:8183 using AcceptFilter none
++++
+AcceptFilter https none
+AcceptFilter http none
++++
+So goods
--- servlet/src/main/java/org/jfclere/JBCS534/Servlet.java
+++ servlet/src/main/java/org/jfclere/JBCS534/Servlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import java.util.Date;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,12 +15,23 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(description = "Test servlet for mod_cache", urlPatterns = { "/" })
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static String payload = null;
+	public void init() throws ServletException {
+		StringBuilder load = new StringBuilder(1024);
+		for (int i=0; i<128;i++) {
+			load.append("ABCDEFG");
+		}
+		payload = load.toString();
+	}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String title = "mod_cache test servlet";
 		response.setHeader("ETag","W/\"5581-1509871979000\"");
 		response.setHeader("Expires", "Wed, 21 Oct 2018 07:28:00 GMT");
 		Date date = new Date();
 		response.setHeader("TraceDate", date.toString());
+		Random rand = new Random();
+		int n = rand.nextInt(32) + 1;
+		response.setHeader("TraceSize", ""+n);
 
 		PrintWriter out = response.getWriter();
 		out.println("<title>" + title + "</title>");
@@ -27,6 +39,9 @@ public class Servlet extends HttpServlet {
 		out.println("<body>");
 
 		out.println("<h3>" + title + "</h3>");
+		for (int j=0; j<n; j++) {
+			out.print(payload);
+		}
 
        		out.println("</body>");
         	out.println("</html>");
